ea46de44685b9c863c403cc4c7b6216db43199b8
Steven <stevenmaxwell199@gmail.com>
2026-05-25 01:21:57 -0400

    fix: change apiPrefix to gateway to avoid Vercel /api/* interception

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,6 +12,7 @@ return $appClass::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         api: __DIR__.'/../routes/api.php',
+        apiPrefix: 'gateway',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
